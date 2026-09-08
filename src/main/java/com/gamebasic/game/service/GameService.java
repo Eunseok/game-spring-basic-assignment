7e9b7bd98144afe1e93dc967b5e96be00322bbcb
Eunseok <79277772+Eunseok@users.noreply.github.com>
2026-09-08 18:26:25 +0900

    Lv 7. 목록·상세 조회: 저장된 여정 이어하기  (#7)
    * feat: JpaAuditing 설정 및 BaseTimeEntity 구현
    
    * feat: 게임 목록 조회 구현
    
    * feat: 게임 상세 조회 구현
    
    * refactor: 게임 목록조회 ID기준 내림차순 쿼리메서드 변경

--- src/main/java/com/gamebasic/game/service/GameService.java
+++ src/main/java/com/gamebasic/game/service/GameService.java
@@ -2,6 +2,7 @@ package com.gamebasic.game.service;
 
 import com.gamebasic.game.dto.CreateRequest;
 import com.gamebasic.game.dto.GameDetailResponse;
+import com.gamebasic.game.dto.GameSummaryResponse;
 import com.gamebasic.game.dto.ProgressRequest;
 import com.gamebasic.game.entity.Game;
 import com.gamebasic.game.repository.GameRepository;
@@ -17,6 +18,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -86,15 +88,41 @@ public class GameService {
         );
     }
 
-    // TODO (Lv 7): 게임 목록 조회. 주석을 풀고 구현하세요.
-    // @Transactional(readOnly = true)
-    // public List<GameSummaryResponse> getGames() {
-    // }
+    @Transactional(readOnly = true)
+    public List<GameSummaryResponse> getGames() {
+        return gameRepository.findAllGameByOrderByIdDesc().stream()
+                .map(game -> GameSummaryResponse.builder()
+                        .id(game.getId())
+                        .playerName(game.getPlayerName())
+                        .currentFloor(game.getCurrentFloor())
+                        .currentHp(game.getCurrentHp())
+                        .phase(String.valueOf(game.getPhase()))
+                        .status(String.valueOf(game.getStatus()))
+                        .deckSize(runCardRepository.findAllByGameOrderByIdAsc(game).size())
+                        .createAt(game.getCreatedAt())
+                        .updateAt(game.getUpdateAt())
+                        .build())
+                .collect(Collectors.toList());
+    }
 
-    // TODO (Lv 7): 게임 상세 조회. 주석을 풀고 구현하세요.
-    // @Transactional(readOnly = true)
-    // public GameDetailResponse getGame(Long gameId) {
-    // }
+     @Transactional(readOnly = true)
+     public GameDetailResponse getGame(Long gameId) {
+        Game game = findGame(gameId);
+         List<RunCard> cards = runCardRepository.findAllByGameOrderByIdAsc(game);
+         List<CardResponse> deck = new ArrayList<>();
+         for (RunCard card : cards) {
+             deck.add(new CardResponse(card.getId(), card.getCardType(), card.getAcquiredFloor()));
+         }
+         return new GameDetailResponse(
+                 game.getId(),
+                 game.getPlayerName(),
+                 game.getCurrentHp(),
+                 game.getCurrentFloor(),
+                 game.getPhase(),
+                 game.getStatus(),
+                 deck
+         );
+     }
 
     // TODO (Lv 8): 플레이어 이름 변경 — 변경 감지로 수정
     // TODO (Lv 8): 게임 삭제
