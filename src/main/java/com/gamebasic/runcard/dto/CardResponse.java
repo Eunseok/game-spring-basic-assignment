6dcfb8851a36fad71f3192fc26db1fbe6038b96e
Eunseok <asp6742@gmail.com>
2026-09-08 15:16:57 +0900

    feat: CardResponse 필드추가(id,cardType,aquiredFloor) 및 생성자 @AllArgsConstructor로 대체

--- src/main/java/com/gamebasic/runcard/dto/CardResponse.java
+++ src/main/java/com/gamebasic/runcard/dto/CardResponse.java
@@ -1,11 +1,12 @@
 package com.gamebasic.runcard.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class CardResponse {
-    // TODO (Lv 5): API 명세의 카드 응답 JSON에 맞게 필드를 만들고 생성자에서 채우세요.
-
-    public CardResponse(Long id, String cardType, int acquiredFloor) {
-    }
+    private Long id;
+    private String cardType;
+    private int acquiredFloor;
 }
