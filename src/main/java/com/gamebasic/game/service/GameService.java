a850553ad7532de5e62211d7f4bfbf03a047d63e
Eunseok <asp6742@gmail.com>
2026-09-09 10:44:29 +0900

    fix: 이미 끝난 게임의 기록을 덮어씌울수 있던 이슈 해결

--- src/main/java/com/gamebasic/game/service/GameService.java
+++ src/main/java/com/gamebasic/game/service/GameService.java
@@ -59,6 +59,10 @@ public class GameService {
 
     public GameDetailResponse updateProgress(Long gameId, ProgressRequest request) {
         Game game = findGame(gameId);
+        if(game.isFinished()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "이미 종료된 게임입니다.");
+        }
+
         game.updateProgress(
                 request.getCurrentHp(),
                 request.getCurrentFloor(),
