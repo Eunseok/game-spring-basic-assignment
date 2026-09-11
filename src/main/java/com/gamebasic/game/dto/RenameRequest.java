5d7ff0a69ae76877c8e2f67cb6ba67e1c76c8ace
Eunseok <79277772+Eunseok@users.noreply.github.com>
2026-09-11 15:51:23 +0900

    fix: 리네임시 NULL값이 허용되는 문제 해결 (#14)

--- src/main/java/com/gamebasic/game/dto/RenameRequest.java
+++ src/main/java/com/gamebasic/game/dto/RenameRequest.java
@@ -1,5 +1,6 @@
 package com.gamebasic.game.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,6 +9,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class RenameRequest {
 
+    @NotBlank
     @Size(min = 2, max = 12, message = "플레이어 이름은 2자 이상 12자 이하여야 합니다.")
     private String playerName;
 }
